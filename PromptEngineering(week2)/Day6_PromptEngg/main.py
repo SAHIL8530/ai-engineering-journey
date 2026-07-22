353797226cf1d580ae6b92892083db30a1309697
SAHIL SHINDE <sahilvshinde2005@gmail.com>
2026-07-22 16:23:15 +0530

    Add files via upload

---
+++ PromptEngineering(week2)/Day6_PromptEngg/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from day6-promptengg!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ PromptEngineering(week2)/Day7_react.py/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from day7-react-py!")
+
+
+if __name__ == "__main__":
+    main()
